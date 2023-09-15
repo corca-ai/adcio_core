1c6436525c6257130371dc6304ece079867d8fdf
최민재 <cmjcmj1230@gmail.com>
2023-09-15 12:26:18 +0900

    style: detail show and test init str

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    AdcioCore.init("asdfasdf");
+    AdcioCore.init("init adcio_core");
 
     return MaterialApp(
       home: Scaffold(
--- lib/adcio_core.dart
+++ lib/adcio_core.dart
@@ -1,4 +1,4 @@
 library adcio_core;
 
-export 'src/init_adcio.dart';
+export 'src/init_adcio.dart' show getSessionId, getDeviceId, AdcioCore;
 export 'src/adcio_device_info.dart';

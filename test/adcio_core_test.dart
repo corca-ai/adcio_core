0513f6193313f7e64d1a8804ee170a44188eebae
최민재 <cmjcmj1230@gmail.com>
2023-09-19 17:18:59 +0900

    test: AdcioCore test code

--- test/adcio_core_test.dart
+++ test/adcio_core_test.dart
@@ -1,32 +1,42 @@
 import 'package:adcio_core/adcio_core.dart';
-import 'package:adcio_core/src/adcio_core_interface.dart';
-import 'package:adcio_core/src/error.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'adcio_core_mock.dart';
 
-/// FIXME: FakeAdcioCore 가 아닌 ADCIO core 에서 initializeApp 함수를 호출하는 방향으로 하시죠!
 void main() async {
   test(
       'Exception when requesting getter before calling initializeApp function.',
       () {
+    // action
+    expect(
+      () => AdcioCore.clientId,
+      throwsA(isInstanceOf<UnInitializedException>()),
+    );
+    expect(
+      () => AdcioCore.storeId,
+      throwsA(isInstanceOf<UnInitializedException>()),
+    );
+    expect(
+      () => AdcioCore.sessionId,
+      throwsA(isInstanceOf<UnInitializedException>()),
+    );
+    expect(
+      () => AdcioCore.deviceId,
+      throwsA(isInstanceOf<UnInitializedException>()),
+    );
+  });
+
+  test('Error when requesting setter before calling initializeApp function.',
+      () {
     // define
-    AdcioCoreInterface fakeInitTestAdcioCore = FakeAdcioCore();
+    const String sampleDeviceId = "6D79D039-3FE3-4887-B0BC-FDDCBD758C99";
+    const String sampleSessionId = "a3e0efcc-bbed-4c73-b001-cd3d0c54e7a6";
 
     // action
     expect(
-      () => fakeInitTestAdcioCore.clientId,
+      () => AdcioCore.deviceId = sampleDeviceId,
       throwsA(isInstanceOf<UnInitializedException>()),
     );
     expect(
-      () => fakeInitTestAdcioCore.storeId,
-      throwsA(isInstanceOf<UnInitializedException>()),
-    );
-    expect(
-      () => fakeInitTestAdcioCore.sessionId,
-      throwsA(isInstanceOf<UnInitializedException>()),
-    );
-    expect(
-      () => fakeInitTestAdcioCore.deviceId,
+      () => AdcioCore.sessionId = sampleSessionId,
       throwsA(isInstanceOf<UnInitializedException>()),
     );
   });
@@ -34,83 +44,58 @@ void main() async {
   test('A code case that works normally.', () {
     // define
     const String sampleClientId = "djfmdje-djnfjd-qldmf";
-    AdcioCoreInterface fakeSuccessTestAdcioCore = FakeAdcioCore();
+    const String sampleDeviceId = "6D79D039-3FE3-4887-B0BC-FDDCBD758C99";
+    const String sampleSessionId = "a3e0efcc-bbed-4c73-b001-cd3d0c54e7a6";
 
     // set
-    fakeSuccessTestAdcioCore.initializeApp(sampleClientId);
+    AdcioCore.initializeApp(
+      clientId: sampleClientId,
+      deviceId: sampleDeviceId,
+      sessionId: sampleSessionId,
+    );
 
     // action
-    expect(fakeSuccessTestAdcioCore.clientId, sampleClientId);
-    expect(fakeSuccessTestAdcioCore.deviceId,
-        "6D79D039-3FE3-4887-B0BC-FDDCBD758C99");
-    expect(fakeSuccessTestAdcioCore.sessionId,
-        "a3e0efcc-bbed-4c73-b001-cd3d0c54e7a6");
-    expect(fakeSuccessTestAdcioCore.storeId, sampleClientId);
+    expect(AdcioCore.clientId, sampleClientId);
+    expect(AdcioCore.deviceId, sampleDeviceId);
+    expect(AdcioCore.sessionId, sampleSessionId);
+    expect(AdcioCore.storeId, sampleClientId);
   });
 
   test('clientId and storeId must always be the same.', () {
     // define
     const String sampleClientId = "djfmdje-djnfjd-qldmf";
-    AdcioCoreInterface fakeClientStoreIdTestAdcioCore = FakeAdcioCore();
+    const String sampleDeviceId = "6D79D039-3FE3-4887-B0BC-FDDCBD758C99";
+    const String sampleSessionId = "a3e0efcc-bbed-4c73-b001-cd3d0c54e7a6";
 
     // set
-    fakeClientStoreIdTestAdcioCore.initializeApp(sampleClientId);
-
-    // action
-    expect(fakeClientStoreIdTestAdcioCore.clientId,
-        fakeClientStoreIdTestAdcioCore.storeId);
-  });
-
-  test('sessionId must always be constant during runtime.', () {
-    // define
-    AdcioCoreInterface fakeSessionTestAdcioCore = FakeAdcioCore();
-    fakeSessionTestAdcioCore.initializeApp("Sample Client ID");
-    final List<String> sessionIds = [];
-
-    // set
-    for (int i = 0; i < 10; i++) {
-      sessionIds.add(fakeSessionTestAdcioCore.sessionId);
-    }
-
-    // action
-    for (int i = 1; i < 10; i++) {
-      expect(sessionIds[i - 1], sessionIds[i]);
-    }
-  });
-
-  test('Error when requesting setter before calling initializeApp function.',
-      () {
-    // define
-    AdcioCoreInterface fakeSetInitTestAdcioCore = FakeAdcioCore();
-    const String sampleDeviceId = "fjenxje-dnfbd-djend0-dndnew";
-    const String sampleSessionId = "djen-dnms-denc-nehj-mqmdjx";
-
-    // action
-    expect(
-      () => fakeSetInitTestAdcioCore.deviceId = sampleDeviceId,
-      throwsA(isInstanceOf<UnInitializedException>()),
+    AdcioCore.initializeApp(
+      clientId: sampleClientId,
+      deviceId: sampleDeviceId,
+      sessionId: sampleSessionId,
     );
-    expect(
-      () => fakeSetInitTestAdcioCore.sessionId = sampleSessionId,
-      throwsA(isInstanceOf<UnInitializedException>()),
-    );
+
+    // action
+    expect(AdcioCore.clientId, AdcioCore.storeId);
   });
 
   test('change the value using the setter', () {
     // define
     const String sampleClientId = "djfmdje-djnfjd-qldmf";
-    const String sampleDeviceId = "fjenxje-dnfbd-djend0-dndnew";
-    const String sampleSessionId = "djen-dnms-denc-nehj-mqmdjx";
-
-    AdcioCoreInterface fakeSetterTestAdcioCore = FakeAdcioCore();
+    const String sampleDeviceId = "6D79D039-3FE3-4887-B0BC-FDDCBD758C99";
+    const String sampleSessionId = "a3e0efcc-bbed-4c73-b001-cd3d0c54e7a6";
 
     // set
-    fakeSetterTestAdcioCore.initializeApp(sampleClientId);
-    fakeSetterTestAdcioCore.deviceId = sampleDeviceId;
-    fakeSetterTestAdcioCore.sessionId = sampleSessionId;
+    AdcioCore.initializeApp(
+      clientId: sampleClientId,
+      deviceId: sampleDeviceId,
+      sessionId: sampleSessionId,
+    );
+
+    AdcioCore.deviceId = sampleDeviceId;
+    AdcioCore.sessionId = sampleSessionId;
 
     // action
-    expect(fakeSetterTestAdcioCore.deviceId, sampleDeviceId);
-    expect(fakeSetterTestAdcioCore.sessionId, sampleSessionId);
+    expect(AdcioCore.deviceId, sampleDeviceId);
+    expect(AdcioCore.sessionId, sampleSessionId);
   });
 }

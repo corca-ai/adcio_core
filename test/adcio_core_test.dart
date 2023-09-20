2ea358d685251ff9a8555dd9cae6ee8c87b5c338
adbr <adbr.dev@gmail.com>
2023-09-20 15:48:57 +0900

    feat: add FetchDeviceId to mock

--- test/adcio_core_test.dart
+++ test/adcio_core_test.dart
@@ -1,7 +1,23 @@
 import 'package:adcio_core/adcio_core.dart';
+@GenerateNiceMocks([MockSpec<FetchDeviceId>()])
+import 'package:adcio_core/src/fetch_device_id.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'adcio_core_test.mocks.dart';
 
 void main() async {
+  final FetchDeviceId fetchDeviceId = MockFetchDeviceId();
+
+  setUp(() {
+    when(
+      fetchDeviceId.call(),
+    ).thenAnswer(
+      (_) async => "DEVICE_UNIQUE_ID",
+    );
+  });
+
   test(
       'Exception when requesting getter before calling initializeApp function.',
       () {

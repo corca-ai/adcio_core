a142ccb1b68b7380a76177a1ce509a7a337faa84
yoohyunmyeong <aahspringaa4@naver.com>
2024-02-15 15:14:01 +0900

    refactor: remove don't use code

--- lib/src/error.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/services.dart';
-
-class UnInitializedException extends PlatformException {
-  UnInitializedException({
-    super.code = 'UNINITIALIZED',
-    super.message = 'The plugin has not been initialized properly.',
-  });
-}

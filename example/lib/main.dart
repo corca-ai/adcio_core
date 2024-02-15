441938a3564e67f23e1397b7f7c01b3189c08545
yoohyunmyeong <aahspringaa4@naver.com>
2024-02-15 15:29:28 +0900

    feat: update core example app code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,10 +7,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  /// this is important to call `AdcioCore.initializeApp(clientId: 'ADCIO_STORE_ID')` function.
-  await AdcioCore.initializeApp(
-      clientId: 'f8f2e298-c168-4412-b82d-98fc5b4a114a');
-
   runApp(const MainApp());
 }
 
@@ -29,15 +25,9 @@ class MainApp extends StatelessWidget {
                 children: [
                   TextButton(
                     onPressed: () async {
-                      /// You can get the device id through the AdcioCore() object.
-                      /// Of course, you can also set the device id.
-                      /// ```dart
-                      /// String deviceId = AdcioCore.deviceId;
-                      /// AdcioCore.deviceid = "DEVICE_UNIQUE_ID";
-                      /// String deviceId = AdcioCore.deviceId; // result: DEVICE_UNIQUE_ID
-                      /// ```
-
-                      final String deviceId = AdcioCore.deviceId;
+                      /// You can get the device id through the FetchDeviceId() object.
+                      final fetchDeviceId = FetchDeviceId();
+                      final String deviceId = await fetchDeviceId();
                       Fluttertoast.showToast(msg: deviceId);
                     },
                     style: TextButton.styleFrom(
@@ -47,15 +37,8 @@ class MainApp extends StatelessWidget {
                   ),
                   TextButton(
                     onPressed: () {
-                      /// You can get the session id through the AdcioCore() object.
-                      /// Of course, you can also set the session id.
-                      /// ```dart
-                      /// String sessionId = AdcioCore.sessionId;
-                      /// AdcioCore.sessionId = "RUNTIME_SESSION_ID";
-                      /// String sessionId = AdcioCore.sessionId; // result: RUNTIME_SESSION_ID
-                      /// ```
-
-                      final String sessionId = AdcioCore.sessionId;
+                      /// You can get the session id through the SessionClient() object.
+                      final String sessionId = SessionClient().loadSessionId();
                       Fluttertoast.showToast(msg: sessionId);
                     },
                     style: TextButton.styleFrom(
@@ -63,42 +46,10 @@ class MainApp extends StatelessWidget {
                     ),
                     child: const Text("get Session Id"),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      /// You can get the client id through the AdcioCore() object.
-                      /// clientId is initialized with the value entered as a parameter while you call the initializeApp() function.
-                      /// ```dart
-                      /// String clientId = AdcioCore.clientId;
-                      /// ```
-
-                      final String clientId = AdcioCore.clientId;
-                      Fluttertoast.showToast(msg: clientId);
-                    },
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.blue,
-                    ),
-                    child: const Text("get Client Id"),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      /// You can get the store id through the AdcioCore() object.
-                      /// storeId has the same value as clientId.
-                      /// ```dart
-                      /// String storeId = AdcioCore.storeId;
-                      /// ```
-
-                      final String clientId = AdcioCore.storeId;
-                      Fluttertoast.showToast(msg: clientId);
-                    },
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.green,
-                    ),
-                    child: const Text("get Store Id"),
-                  ),
                 ],
               ),
               const Expanded(
-                child: Column(
+                child: Row(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [

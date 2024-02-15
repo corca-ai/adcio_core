79f0c5c7a81da739643ed6e8d41467a0f0b244b1
yoohyunmyeong <aahspringaa4@naver.com>
2024-02-15 18:41:05 +0900

    test: update core test code

--- test/adcio_core_test.dart
+++ test/adcio_core_test.dart
@@ -1,6 +1,6 @@
-@GenerateNiceMocks([MockSpec<FetchDeviceId>()])
-import 'package:adcio_core/src/fetch_device_id.dart';
-import 'package:adcio_core/src/session_client.dart';
+@GenerateNiceMocks([MockSpec<DeviceIdentifier>()])
+import 'package:adcio_core/src/device_identifier.dart';
+import 'package:adcio_core/src/session_identifier.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -8,32 +8,32 @@ import 'package:mockito/mockito.dart';
 import 'adcio_core_test.mocks.dart';
 
 void main() async {
-  final FetchDeviceId fetchDeviceId = MockFetchDeviceId();
+  final DeviceIdentifier fetchDeviceId = MockDeviceIdentifier();
 
   setUp(() {
     when(
-      fetchDeviceId.call(),
+      fetchDeviceId.loadId(),
     ).thenAnswer(
       (_) async => "DEVICE_UNIQUE_ID",
     );
   });
 
   test('A code case that works normally.', () async {
-    final String deviceId = await fetchDeviceId();
-    final String sessionId = SessionClient().loadSessionId();
+    final String deviceId = await fetchDeviceId.loadId();
+    final String sessionId = SessionIdentifier().loadId();
 
     // action
     expect("DEVICE_UNIQUE_ID", deviceId);
-    expect(SessionClient().loadSessionId(), sessionId);
+    expect(SessionIdentifier().loadId(), sessionId);
   });
 
   test('Verifying if the sessionId remains the same during runtime', () async {
     // verify1
-    final sessionId = SessionClient().loadSessionId();
-    expect(sessionId, SessionClient().loadSessionId());
+    final sessionId = SessionIdentifier().loadId();
+    expect(sessionId, SessionIdentifier().loadId());
 
     // verify2
     Future.delayed(const Duration(seconds: 2));
-    expect(sessionId, SessionClient().loadSessionId());
+    expect(sessionId, SessionIdentifier().loadId());
   });
 }
